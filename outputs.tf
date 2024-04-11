b1fa5f88aad49263d9bc4e9e890c735e09aaff21
saikrishnama <>
2024-04-11 15:45:15 -0400

    github workflows

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "kms_key_id" {
+  description = "KMS Key id"
+  value       = aws_kms_key.kms_key[0].id
+
+}
+output "kms_arn" {
+  description = "KMS arn value"
+  value       = aws_kms_key.kms_key[0].arn
+}
